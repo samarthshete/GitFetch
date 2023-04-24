3c57b0ce891038fc4391687b0b597a7705026e89
samarth <samarthshete14@gmail.com>
2023-04-24 17:33:03 +0530

    first commit

---
+++ src/assets/wrapper/RepoStats.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+  div {
+    width: 100% !important;
+  }
+
+  h3 {
+    text-align: center;
+    margin-top: 0;
+  }
+  .statsContainer {
+    display: grid;
+    grid-template-columns: auto auto;
+    gap: 2rem 3rem;
+  }
+`;
+
+export default Wrapper;
