2176f35107345f848e7007d0f29b5289697a09e6
samarth <samarthshete14@gmail.com>
2023-04-30 17:43:20 +0530

    first commit

--- src/Components/RepoContainer.js
+++ src/Components/RepoContainer.js
@@ -8,18 +8,24 @@ export default function RepoContainer() {
   const { repos } = useAppContext();
   const [showDropdown, setShowDropdown] = useState(false);
   const [sortOption, setSortOption] = useState("");
-  const handleSortOption =( option) =>{
-    setShowDropdown(false);
-    setSortOption(option);
-  }
 
-  const sortedRepos  = () =>{
-      
+  let sorted = [...repos];
+
+  if (sortOption === "star") {
+    sorted.sort((a, b) => b.stargazers_count - a.stargazers_count);
+  } else if (sortOption === "fork") {
+    sorted.sort((a, b) => b.forks_count - a.forks_count);
+  } else {
+    sorted.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
   }
+  const handleSortOptionClick = (option) => {
+    setSortOption(option);
+    setShowDropdown(false);  
+  };
   return (
     <section className="section">
       <Wrapper className="section-center">
-      <div className="repo-title">
+        <div className="repo-title">
           <h3> Repository</h3>
           <div className="btn-container">
             <button
@@ -33,17 +39,16 @@ export default function RepoContainer() {
               className={showDropdown ? "dropdown show-dropdown" : "dropdown"}
             >
               <ul>
-                <li onClick={() => { handleSortOption("star")}}>Latest</li>
-                <li>Most Star</li>
-                <li>Most Fork</li>
-                 
+                <li onClick={() => handleSortOptionClick("date")}>Latest</li>
+                <li onClick={() => handleSortOptionClick("star")}>Most Star</li>
+                <li onClick={() => handleSortOptionClick("fork")}>Most Fork</li>
               </ul>
             </div>
           </div>
         </div>
         <div>
           <div className="repo-content">
-            {repos.map((repo) => (
+            {sorted.map((repo) => (
               <Repo key={repo.id} {...repo} />
             ))}
           </div>
